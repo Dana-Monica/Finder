378213617f374decb7b871b5227aad3ab9db40e4
Monica Tanase <monicatanase96@yahoo.com>
2021-04-01 21:25:48 +0300

    API not found yet

--- app/src/main/java/com/example/finder/IngredientsActivity.java
+++ app/src/main/java/com/example/finder/IngredientsActivity.java
@@ -2,12 +2,17 @@ package com.example.finder;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -24,6 +29,8 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -31,6 +38,8 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.net.*;
@@ -40,6 +49,8 @@ public class IngredientsActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView imageView;
     private SharedPreferences sp;
+    private String filePath = "";
+    private File photoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +73,7 @@ public class IngredientsActivity extends AppCompatActivity {
     {
         try {
             // on new thread, not on main thread !
-            new RetrieveTask().execute();
+            new RetrieveTask().execute(photoFile);
             Toast.makeText(IngredientsActivity.this, "so far so good", Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
             e.printStackTrace();
@@ -73,10 +84,48 @@ public class IngredientsActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
+            /*Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
+            imageView.setImageBitmap(imageBitmap);*/
             searchPicture();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        filePath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            photoFile  = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.finder",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
 
@@ -101,15 +150,6 @@ public class IngredientsActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/com/example/finder/RetrieveTask.java
+++ app/src/main/java/com/example/finder/RetrieveTask.java
@@ -1,15 +1,22 @@
 package com.example.finder;
 
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,37 +24,51 @@ public class RetrieveTask extends AsyncTask {
 
     private HttpURLConnection myConnection;
     private URL myURL;
-    private String userToken = "439e2e4d7bc0ca1d2e9601f5b65cdd199c5a130b";
-    private OutputStream out = null;
+    private String userToken = "439e2e4d7bc0ca1d2e9601f5b65cdd199c5a130b", serverResponseMessage;
+    private DataOutputStream out = null;
     private Map<String, String> parameters;
+    private File photoFile;
+    private int serverResponseCode = 0;
 
     @Override
     protected Object doInBackground(Object... objects) {
         try {
+            photoFile = (File) objects[0];
             myURL = new URL("https://api.logmeal.es/v2/recognition/dish");
             myConnection = (HttpURLConnection) myURL.openConnection();
             myConnection.setRequestMethod("POST");
-            myConnection.setRequestProperty("Content-Type", "application/json; utf-8");
+            myConnection.setRequestProperty("Content-Type", "multipart/form-data");
             myConnection.setRequestProperty("Accept", "application/json");
+            myConnection.setRequestProperty("Authorization", "Bearer " + userToken);
 
             parameters = new HashMap<>();
             parameters.put("Authorization", "Bearer " + userToken);
             parameters.put("model_version", "v0.9");
             parameters.put("skip_types", "[1,3]");
             parameters.put("language", "eng");
+            //parameters.put("image", photoFile);
 
             myConnection.setDoOutput(true); // folosim POST
-            OutputStream os = myConnection.getOutputStream();
+            out = new DataOutputStream(myConnection.getOutputStream());
             byte[] intput = ParameterStringBuilder.getParamsString(parameters).getBytes("utf-8");
-            os.write(intput,0,intput.length);
+            //out.write(intput,0,intput.length);
+
+            byte[] fileData = new byte[(int)photoFile.length()];
+            FileInputStream in = new FileInputStream(photoFile);
+            in.read(fileData);
+            in.close();
+            out.write(fileData,0,fileData.length);
 
             /*BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
             writer.write(ParameterStringBuilder.getParamsString(parameters));
             writer.flush();
-            writer.close();
+            writer.close();*/
             out.close();
-*/
+
             myConnection.connect();
+
+            serverResponseCode = myConnection.getResponseCode();
+            serverResponseMessage = myConnection.getResponseMessage();
             // handle cookies?
         } catch (Exception e) {
             e.printStackTrace();
